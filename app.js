1d232e0a772b967be61d3631c2164c3ff1f6da4a
DanielDees <explorers2010@hotmail.com>
2018-04-28 17:09:24 -0400

    Game now runs

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-"use strict";
+;"use strict";
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var FPS = 60;
+
+
+function gameLoop() {
+
+	ctx.fillStyle = "blue";
+	ctx.fillRect(20, 20, 50, 50);
+
+}
+
+//Run game
+var app = setInterval(gameLoop, (1000 / FPS));
